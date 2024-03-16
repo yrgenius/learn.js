33d29166646cdd3e461a13953bb96c4cc1d6ae41
yrgenius <yrgenius80@gmail.com>
2024-03-16 22:20:46 +0900

    add lesson 2.17

---
+++ Chapter_1/2/2.17.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Замените код Function Expression стрелочной функцией
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no()
+}
+
+ask(
+    "Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert("Вы отменили выполнение.")
+)
